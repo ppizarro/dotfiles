d36f7ceb36f4132675c375c978d814130691a3f4
paulo.pizarro <paulo.pizarro@neoway.com.br>
2018-11-27 17:37:56 -0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
+DIR=`pwd`
 
 #Tmux configuration
-echo "Creating backups of any current tmux configurations..."
-cp ~/.tmux.conf ~/.tmux.conf.OLD
-
 echo "Installing my tmux configuration..."
-cp tmux/tmux.conf ~/.tmux.conf
+rm -f ~/.tmux.conf
+ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
 
 #Xdefaults
-echo "Creating backup of ~/.Xdefaults..."
-cp ~/.Xdefaults ~/.Xdefaults.OLD
-
 echo "Installing my Xdefault configuration..."
-cp URxvt/Xdefaults ~/.Xdefaults
+rm -f ~/.xinitrc
+ln -s $PWD/xorg/xinitrc ~/.xinitrc
+rm -f ~/.Xdefaults
+ln -s $PWD/xorg/Xdefaults ~/.Xdefaults
 
 #i3
-echo "Creating backup of ~/.config/i3/config..."
-mkdir -p ~/.config/i3/config 2> /dev/null
-cp ~/.config/i3/config ~/.config/i3/config.OLD
-cp ~/.i3blocks.conf ~/.i3blocks.conf.OLD
-
 echo "Installing my i3 configuration..."
-cp i3/config ~/.config/i3/config 
-cp i3/i3blocks.conf ~/.i3blocks.conf
+mkdir -p ~/.config/i3 2> /dev/null
+rm -f ~/.config/i3/config 
+ln -s $PWD/i3/config ~/.config/i3/config
+
+mkdir -p ~/.config/i3status 2> /dev/null
+rm -f ~/.config/i3status/config 
+ln -s $PWD/i3/i3status.conf ~/.config/i3status/config
+
+rm -f ~/.i3blocks.conf
+ln -s $PWD/i3/i3blocks.conf ~/.i3blocks.conf
 
 echo ""
